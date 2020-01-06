4d0f3af420a48dd7403b01ee932994b60d28db7d
cony ross <jardlml1209@gmail.com>
2020-01-06 13:25:19 -0300

    Añadido categoria en model

--- final-backend/src/main/java/com/example/demo/model/Category.java
+++ final-backend/src/main/java/com/example/demo/model/Category.java
@@ -1,4 +1,44 @@
 package com.example.demo.model;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="CATEGORIES")
 public class Category {
+
+    @Column(name="ID", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="NAME", nullable = false)
+    private String name;
+
+    @Column(name="DESCRIPTION")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
