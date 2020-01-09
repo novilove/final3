94e62c2a563745d2d6da9d363928f63a9ee84385
Javier Rojas <jardlml1209@gmail.com>
2020-01-09 16:09:50 -0300

    avance

---
+++ Final-Back/final-backend/src/main/java/com/example/demo/Dto/DtoCategory.java
@@ -0,0 +1,22 @@
+package com.example.demo.Dto;
+
+public class DtoCategory {
+    private String nameDto;
+    private String descriptionDto;
+
+    public String getNameDto() {
+        return nameDto;
+    }
+
+    public void setNameDto(String nameDto) {
+        this.nameDto = nameDto;
+    }
+
+    public String getDescriptionDto() {
+        return descriptionDto;
+    }
+
+    public void setDescriptionDto(String descriptionDto) {
+        this.descriptionDto = descriptionDto;
+    }
+}
---
+++ Final-Back/final-backend/src/main/java/com/example/demo/Dto/EventDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.Dto;
+
+public class EventDto {
+    private Long idDto;
+    private String nameDto;
+    private Integer durationDto;
+    private String descriptionDto;
+    private String timeDto;
+    private String dateDto;
+    private Integer capacityDto;
+    private String categoryDto;
+    private String placeDto;
+    //private String categoryDto;
+
+}
