2a2da1874c357d08a3ccd572c0e83166be98d203
AkaneGraves <akanne.graves@gmail.com>
2020-01-10 00:58:49 -0300

    event service test

---
+++ Final-Back/final-backend/src/main/java/com/example/demo/service/EventService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public interface EventService {
+
+  /*  Event createEvent(DtoEvent events) throws Exception;
+    Boolean deleteEvent(long id) throws Exception;
+*/
+
+}
