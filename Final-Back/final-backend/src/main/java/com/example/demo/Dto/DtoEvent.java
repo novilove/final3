a919dac5c941c51f42260d656d40949b676b0bee
Javier Rojas <jardlml1209@gmail.com>
2020-01-10 00:35:11 -0300

    dtoEvent agregado espero este bien jeje

---
+++ Final-Back/final-backend/src/main/java/com/example/demo/Dto/DtoEvent.java
@@ -0,0 +1,78 @@
+package com.example.demo.Dto;
+
+
+
+public class DtoEvent {
+    private String nameDto;
+    private Integer durationDto;
+    private String descriptionDto;
+    private String timeDto;
+    private String dateDto;
+    private Integer capacityDto;
+    private String namePlaceDto;
+    private String nameCategotyDto;
+
+    public String getNameDto() {
+        return nameDto;
+    }
+
+    public void setNameDto(String nameDto) {
+        this.nameDto = nameDto;
+    }
+
+    public Integer getDurationDto() {
+        return durationDto;
+    }
+
+    public void setDurationDto(Integer durationDto) {
+        this.durationDto = durationDto;
+    }
+
+    public String getDescriptionDto() {
+        return descriptionDto;
+    }
+
+    public void setDescriptionDto(String descriptionDto) {
+        this.descriptionDto = descriptionDto;
+    }
+
+    public String getTimeDto() {
+        return timeDto;
+    }
+
+    public void setTimeDto(String timeDto) {
+        this.timeDto = timeDto;
+    }
+
+    public String getDateDto() {
+        return dateDto;
+    }
+
+    public void setDateDto(String dateDto) {
+        this.dateDto = dateDto;
+    }
+
+    public Integer getCapacityDto() {
+        return capacityDto;
+    }
+
+    public void setCapacityDto(Integer capacityDto) {
+        this.capacityDto = capacityDto;
+    }
+
+    public String getNamePlaceDto() {
+        return namePlaceDto;
+    }
+
+    public void setNamePlaceDto(String namePlaceDto) {
+        this.namePlaceDto = namePlaceDto;
+    }
+
+    public String getNameCategotyDto() {
+        return nameCategotyDto;
+    }
+
+    public void setNameCategotyDto(String nameCategotyDto) {
+        this.nameCategotyDto = nameCategotyDto;
+    }
+}
