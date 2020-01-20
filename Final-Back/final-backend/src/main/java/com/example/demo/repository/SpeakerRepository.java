003cd731157b24c6c067d5ca3e919e9cf2e3a70e
Javier Rojas <jardlml1209@gmail.com>
2020-01-20 13:44:30 -0300

    Avance 2 talk imp

--- Final-Back/final-backend/src/main/java/com/example/demo/repository/SpeakerRepository.java
+++ Final-Back/final-backend/src/main/java/com/example/demo/repository/SpeakerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SpeakerRepository extends JpaRepository<Speaker,Long> {
    Speaker findByid(Long id);
+   Speaker findByName(String name);
 }
